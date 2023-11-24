2ff3402ebd6c2fdc4ff172b879f8a73202e27672
winqus <67459222+winqus@users.noreply.github.com>
2023-11-24 03:05:09 +0000

    Add Dockerfile and update server.pl

--- drones-planning-prolog-api/src/server.pl
+++ drones-planning-prolog-api/src/server.pl
@@ -4,4 +4,26 @@
 :- use_module('route_handler').
 
 start_server(Port) :-
-    http_server(http_dispatch, [port(Port)]).
+    http_server(http_dispatch, [port(Port)]),
+    server_loop.
+
+server(Port) :-
+    http_server(http_dispatch,
+                [ port(Port),
+                    workers(16)
+                ]).
+
+server_loop :-
+    repeat,
+    thread_get_message(Message),
+    handle_message(Message),
+    Message == stop_server,
+    !.
+
+handle_message(stop_server, Port) :-
+    http_stop_server(Port, []),
+    writeln('Server stopped.').
+
+handle_message(Message) :-
+    % Add handling for other messages as needed
+    writeln('Received unknown message: '), writeln(Message).
