9ebbafe82e584a48ecde57f11f6683e6041e5da3
Arina G. <None>
2023-11-26 18:43:31 +0000

    Merged in technical/front-end-tests (pull request #50)
    Technical - front end tests
    
    * Add create and list floors test
    
    * Add list buildings test
    
    * Add test for the case no buildings exist
    
    
    Approved-by: João R.

--- drones-angular-client/cypress/support/commands.ts
+++ drones-angular-client/cypress/support/commands.ts
@@ -3,15 +3,18 @@
 // with Intellisense and code completion in your
 // IDE or Text Editor.
 // ***********************************************
+
 // declare namespace Cypress {
 //   interface Chainable<Subject = any> {
 //     customCommand(param: any): typeof customCommand;
 //   }
 // }
-//
+
 // function customCommand(param: any): void {
 //   console.warn(param);
 // }
+
+
 //
 // NOTE: You can use it like so:
 // Cypress.Commands.add('customCommand', customCommand);
@@ -29,6 +32,23 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add('login' as any, (username, password) => {
+    cy.visit('/login')
+  
+    cy.get('input[name=username]').type(username as any)
+  
+    // {enter} causes the form to submit
+    cy.get('input[name=password]').type(`${password}{enter}`, { log: false })
+  
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/dashboard')
+  
+    // our auth cookie should be present
+    cy.getCookie('your-session-cookie').should('exist')
+  
+    // UI should reflect this user being logged in
+    cy.get('h1').should('contain', username)
+  })
 //
 //
 // -- This is a child command --
