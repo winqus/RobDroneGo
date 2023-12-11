f63bbd506186836c5e9652713c4e18ac6be35ccb
winqus <67459222+winqus@users.noreply.github.com>
2023-12-11 13:20:28 +0000

    Reorder predicates for readability, add example usage

--- drones-planning-prolog-api/algorithms/genetic/geneticAlgorithm.pl
+++ drones-planning-prolog-api/algorithms/genetic/geneticAlgorithm.pl
@@ -14,14 +14,14 @@ task(t5, 3, 8, 2).
 % tasks(NTasks).
 tasks(5).
 
-% Usage example:
-/*
+%%% Usage example %%%:
+/*-----------------------------------------
   ?- generate.
   Number of new Generations: 10.
   Population Size: |: 20.
   Crossover Probability (%):|: 70.
   Mutation Probability (%):|: 30. 
-*/
+-----------------------------------------*/
 
 
 % Parameterization
@@ -72,11 +72,6 @@ generate_individual(TaskList, NumT, [G|Rest]):-
   NumT1 is NumT-1,
   generate_individual(NewList, NumT1, Rest).
 
-remove(1, [G|Rest], G, Rest).
-remove(N, [G1|Rest], G, [G1|Rest1]):-
-  N1 is N-1,
-  remove(N1, Rest, G, Rest1).
-
 evaluate_population([], []).
 evaluate_population([Ind|Rest], [Ind*V|Rest1]):-
   evaluate(Ind, V),
@@ -99,19 +94,6 @@ evaluate([T|Rest], Inst, V):-
 
 sort_population(PopEval, PopEvalSorted):-
   bubble_sort(PopEval, PopEvalSorted).
-%----------------------------------------------
-bubble_sort([X], [X]) :- !.
-bubble_sort([X|Xs], Ys) :-
-    bubble_sort(Xs, Zs),
-    bubble_swap([X|Zs], Ys).
-
-bubble_swap([X], [X]) :- !.
-
-bubble_swap([X*VX, Y*VY|L1], [Y*VY|L2]) :-
-    VX > VY, !,
-    bubble_swap([X*VX|L1], L2).
-
-bubble_swap([X|L1], [X|L2]) :- bubble_swap(L1, L2).
 
 generate_generation(G, G, Pop) :- !,
     write('Generation '), write(G), write(':'), nl, write(Pop), nl.
@@ -150,6 +132,59 @@ crossover([Ind1* _, Ind2* _|Rest], [NInd1, NInd2|Rest1]) :-
     (NInd1 = Ind1, NInd2 = Ind2)),
     crossover(Rest, Rest1).
 
+cross(Ind1, Ind2, P1, P2, NInd11) :-
+  sublist(Ind1, P1, P2, Sub1),
+  tasks(NumT),
+  R is NumT - P2,
+  rotate_right(Ind2, R, Ind21),
+  eliminate(Ind21, Sub1, Sub2),
+  P3 is P2 + 1,
+  insert(Sub2, Sub1, P3, NInd1),
+  eliminate_h(NInd1, NInd11).
+
+mutation([], []).
+mutation([Ind|Rest], [NInd|Rest1]) :-
+    mutation_probability(Pmut),
+    random(0.0, 1.0, Pm),
+    ((Pm < Pmut, !, mutation1(Ind, NInd)); NInd = Ind),
+    mutation(Rest, Rest1).
+
+mutation1(Ind, NInd) :-
+    generate_crossover_points(P1, P2),
+    mutation22(Ind, P1, P2, NInd).
+
+mutation22([G1|Ind], 1, P2, [G2|NInd]) :-
+    !, P21 is P2 - 1,
+    mutation23(G1, P21, Ind, G2, NInd).
+mutation22([G|Ind], P1, P2, [G|NInd]) :-
+    P11 is P1 - 1, P21 is P2 - 1,
+    mutation22(Ind, P11, P21, NInd).
+
+mutation23(G1, 1, [G2|Ind], G2, [G1|Ind]) :- !.
+mutation23(G1, P, [G|Ind], G2, [G|NInd]) :-
+    P1 is P - 1,
+    mutation23(G1, P1, Ind, G2, NInd).
+
+
+bubble_sort([X], [X]) :- !.
+bubble_sort([X|Xs], Ys) :-
+    bubble_sort(Xs, Zs),
+    bubble_swap([X|Zs], Ys).
+
+bubble_swap([X], [X]) :- !.
+
+bubble_swap([X*VX, Y*VY|L1], [Y*VY|L2]) :-
+    VX > VY, !,
+    bubble_swap([X*VX|L1], L2).
+
+bubble_swap([X|L1], [X|L2]) :- bubble_swap(L1, L2).
+
+%----- Other helper predicates -----------------------------------------------------
+remove(1, [G|Rest], G, Rest).
+remove(N, [G1|Rest], G, [G1|Rest1]):-
+  N1 is N-1,
+  remove(N1, Rest, G, Rest1).
+
 fill_h([], []).
 
 fill_h([_|R1], [h|R2]) :-
@@ -185,7 +220,8 @@ rr(N, [X|R], R2) :-
     N1 is N - 1,
     append(R, [X], R1),
     rr(N1, R1, R2).
-%----------------------------------------------
+
+
 eliminate([], _, []) :- !.
 
 eliminate([X|R1], L, [X|R2]) :-
@@ -208,16 +244,6 @@ insert1(X, N, [Y|L], [Y|L1]) :-
     N1 is N - 1,
     insert1(X, N1, L, L1).
 
-cross(Ind1, Ind2, P1, P2, NInd11) :-
-    sublist(Ind1, P1, P2, Sub1),
-    tasks(NumT),
-    R is NumT - P2,
-    rotate_right(Ind2, R, Ind21),
-    eliminate(Ind21, Sub1, Sub2),
-    P3 is P2 + 1,
-    insert(Sub2, Sub1, P3, NInd1),
-    eliminate_h(NInd1, NInd11).
-
 eliminate_h([], []).
 
 eliminate_h([h|R1], R2) :- !,
@@ -225,26 +251,3 @@ eliminate_h([h|R1], R2) :- !,
 
 eliminate_h([X|R1], [X|R2]) :-
     eliminate_h(R1, R2).
-
-mutation([], []).
-mutation([Ind|Rest], [NInd|Rest1]) :-
-    mutation_probability(Pmut),
-    random(0.0, 1.0, Pm),
-    ((Pm < Pmut, !, mutation1(Ind, NInd)); NInd = Ind),
-    mutation(Rest, Rest1).
-
-mutation1(Ind, NInd) :-
-    generate_crossover_points(P1, P2),
-    mutation22(Ind, P1, P2, NInd).
-
-mutation22([G1|Ind], 1, P2, [G2|NInd]) :-
-    !, P21 is P2 - 1,
-    mutation23(G1, P21, Ind, G2, NInd).
-mutation22([G|Ind], P1, P2, [G|NInd]) :-
-    P11 is P1 - 1, P21 is P2 - 1,
-    mutation22(Ind, P11, P21, NInd).
-
-mutation23(G1, 1, [G2|Ind], G2, [G1|Ind]) :- !.
-mutation23(G1, P, [G|Ind], G2, [G|NInd]) :-
-    P1 is P - 1,
-    mutation23(G1, P1, Ind, G2, NInd).
