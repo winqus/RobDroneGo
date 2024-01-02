88896ac0bdc7126559b6bab3202c5e2e3afaa85f
winqus <67459222+winqus@users.noreply.github.com>
2024-01-02 21:49:05 +0000

    Add routing title updates for history

--- drones-angular-client/src/app/app.component.ts
+++ drones-angular-client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { TEXT_TOKENS as CONTENT } from '../assets/i18n/_textTokens';
 
 @Component({
@@ -9,7 +11,27 @@ import { TEXT_TOKENS as CONTENT } from '../assets/i18n/_textTokens';
 export class AppComponent {
   title = 'dronesangularclient';
   CONTENT = CONTENT;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.updatePageTitle();
+    });
+  }
+
   trackByTitle(index: number, item: any): any {
     return item.title;
+  }
+
+  private updatePageTitle() {
+    let child: any = this.activatedRoute.firstChild;
+    while (child.firstChild) {
+      child = child.firstChild;
+    }
+    if (child.snapshot.data['title']) {
+      document.title = `${child.snapshot.data['title']} | ${CONTENT.title}`;
+    }
   }
 }
